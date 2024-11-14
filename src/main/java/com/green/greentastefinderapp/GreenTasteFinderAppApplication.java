6c94ee0873d81a67bfdb5ed094a1a45a45d9a1ac
hanwoosung <yiok79@naver.com>
2024-11-14 13:58:11 +0900

    푸시 테스트

---
+++ src/main/java/com/green/greentastefinderapp/GreenTasteFinderAppApplication.java
@@ -0,0 +1,13 @@
+package com.green.greentastefinderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenTasteFinderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenTasteFinderAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/greentastefinderapp/GreenTasteFinderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.greentastefinderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenTasteFinderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
